a60930c33f1d19af799c985e7852a815a753a657
Orgil <orgil.u@gmail.com>
2023-03-03 20:05:36 +0800

    snippet fix

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,28 +1,16 @@
 return {
 	{
 		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-			config = function()
-				require("luasnip.loaders.from_vscode").lazy_load()
-			end,
-		},
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
+			-- require("luasnip.loaders.from_vscode").load({
+			-- 	paths = vim.fn.stdpath("config") .. "/snippets",
+			-- })
+		end,
 		opts = {
 			history = true,
 			delete_check_events = "TextChanged",
 		},
-    -- stylua: ignore
-    keys = {
-      {
-        "<tab>",
-        function()
-          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-        end,
-        expr = true, silent = true, mode = "i",
-      },
-      { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
-      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
-    },
 	},
 	{
 		"hrsh7th/nvim-cmp",
@@ -36,6 +24,13 @@ return {
 		},
 		opts = function()
 			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
 			return {
 				completion = {
 					completeopt = "menu,menuone,noinsert",
@@ -43,21 +38,38 @@ return {
 				confirmation = { completeopt = "menu,menuone,noinsert" },
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
+						luasnip.lsp_expand(args.body)
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-u>"] = cmp.mapping.scroll_docs(4),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
 					["<C-d>"] = cmp.mapping.scroll_docs(-4),
 					["<C-c>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<Tab>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					-- ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						elseif cmp.visible() then
+							cmp.confirm({ select = true })
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
-					{ name = "buffer" },
+				}, {
 					{ name = "path" },
+					{ name = "buffer" },
 				}),
 				formatting = {
 					format = function(_, item)
