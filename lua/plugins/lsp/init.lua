2e5d6457f0cd6d06fea0d0874704469b9d5e67af
Orgil <orgil.u@gmail.com>
2023-03-04 12:28:06 +0800

    ts virtual text diagnostic disabled

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -74,6 +74,9 @@ return {
 							result = vim.tbl_islist(result) and result[1] or result
 							vim.lsp.handlers["textDocument/definition"](err, result, ...)
 						end,
+						["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+							virtual_text = false,
+						}),
 					},
 				},
 				clangd = {
